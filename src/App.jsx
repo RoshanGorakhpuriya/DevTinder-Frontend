b70320d9027c3958c61cb35d8a538bfac63febbd
Roshan Gorakhpuriya <roshangorakhpuriya@gmail.com>
2025-02-10 16:09:53 +0100

    setup redux router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import Login from "./Login";
 import Profile from "./Profile";
 import Body from "./Body";
-import { BrowserRouter, Routes , Route }
- from "react-router-dom";
+import {Provider} from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./Feed";
+import { BrowserRouter, Routes , Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Provider store={appStore}>
        <BrowserRouter basename="/">
        <Routes>
         <Route path="/" element={<Body/>}>
+           <Route path="/" element={<Feed/>}/>
            <Route path="/login" element={<Login/>}/>
            <Route path="/profile" element={<Profile/>}/>
         </Route>
        </Routes>
        </BrowserRouter>
+    </Provider>
     </>
   )
 }
