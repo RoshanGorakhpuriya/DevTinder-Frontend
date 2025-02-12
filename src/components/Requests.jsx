d7ee241b1bb83ffa2495970c94f549ee16354f8b
Roshan Gorakhpuriya <roshangorakhpuriya@gmail.com>
2025-02-12 12:17:59 +0100

    Seel all connections Requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addRequests } from "../utils/requestSlice";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Requests = () => {
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+  const fetchRequests = async ()=>{
+    try{
+      const res = await axios.get(BASE_URL + "/user/requests/received" , {
+        withCredentials : true,
+      });
+
+      dispatch(addRequests(res.data.data));
+    }catch(err){
+
+    };
+  }
+
+  useEffect(() => {
+    fetchRequests();
+  } , [])
+  if(!requests) return;
+
+    if(requests.length === 0) return <h1>No Requests Found</h1>
   return (
-    <div>Requests</div>
-  )
+   <div className="text-center my-10"> 
+    <h1 className="text-bold text-white text-3xl">Requests</h1>
+
+        {requests.map((request) => {
+            const { _id , firstName , lastName , photoUrl , age , gender , about} = request.fromUserId;
+
+
+            return (
+            <div key={_id} className=" flex justify-between items-center m-4 p-4 border rounded-lg bg-base-300  w-2/3 mx-auto">
+                <div>
+                <img alt="photo" className="w-20 h-20 rounded-full" src={photoUrl}/>
+                </div>
+                <div className="text-left , mx-10">
+                <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
+                <h2>{"Age :" + age}</h2>
+                <p>{"Gender : " + gender}</p>
+                <p>{"About : " + about}</p>
+                </div>
+                <div>
+                <button className="btn btn-primary mx-2 ">Reject</button>
+                <button className="btn btn-secondary mx-2">Accept</button>
+                </div>
+                
+            </div>
+            );
+        })}
+    </div>
+  );
 }
 
 export default Requests
