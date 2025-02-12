d261f98b43260850b8781294517f099d851d6974
Roshan Gorakhpuriya <roshangorakhpuriya@gmail.com>
2025-02-12 14:40:34 +0100

    Feature : Implement a interested/ignored Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -25,6 +25,9 @@ const Feed = () => {
     useEffect(() => {
         getFeed();
     },[]);
+    if(!feed) return;
+
+    if(feed.length <= 0) return <h1 className="text-center my-4">No new users founds!</h1>
   return (
     feed && (
     <div className="flex justify-center my-10">
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,24 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import {removeUserFromFeed} from "../utils/feedSlice.js" 
 const UserCard = ({ user }) => {
-    console.log(user);
-    const {firstName , lastName , photoUrl , about  , age , gender} = user;
+    const dispatch = useDispatch();
+    const { _id , firstName , lastName , photoUrl , about  , age , gender} = 
+    user;
+    const handleSendRequest = async (status , userId) => {
+      try{
+        const res = await axios.post(
+          BASE_URL + "/request/send/" + status + "/" + userId , {} , {
+            withCredentials : true,
+          }
+        );
+        dispatch(removeUserFromFeed(userId));
+      }
+      catch(err){
+
+      }
+    }
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
   <figure>
@@ -13,8 +31,8 @@ const UserCard = ({ user }) => {
     <p>{"Gender : " + gender}</p>
     <p>{"About  : " + about}</p>
     <div className="card-actions justify-center my-4">
-      <button className="btn btn-primary">Ignore</button>
-      <button className="btn btn-secondary">Interested</button>
+      <button className="btn btn-primary" onClick={() => handleSendRequest("ignored" , _id)}>Ignored</button>
+      <button className="btn btn-secondary" onClick={() => handleSendRequest("interested" , _id)}>Interested</button>
     </div>
   </div>
 </div>
